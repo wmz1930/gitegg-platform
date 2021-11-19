b46740fed2da1083c74825ee9a5ab46d6969b4f8
Melon <wmz1930@users.noreply.github.com>
2021-11-19 20:06:10 +0800

    新增静态文件配置

--- gitegg-platform-oauth2/src/main/java/com/gitegg/platform/oauth2/props/AuthUrlWhiteListProperties.java
+++ gitegg-platform-oauth2/src/main/java/com/gitegg/platform/oauth2/props/AuthUrlWhiteListProperties.java
@@ -8,16 +8,27 @@ import java.util.List;
 
 /**
  * 公共鉴权url配置
+ * @author GitEgg
  */
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "oauth-list")
 public class AuthUrlWhiteListProperties {
 
-    // 白名单配置，不需要鉴权的url
+    /**
+     * 静态文件白名单配置，不需要鉴权的url
+     * 请求经过oauth2时，需要把服务名去掉，这里的配置oauth2不会去掉前缀
+     */
+    private List<String> staticFiles;
+
+    /**
+     * 白名单配置，不需要鉴权的url
+     */
     private List<String> whiteUrls;
 
-    //需要鉴权的公共url配置，不需要单独给所有的角色增加的url
+    /**
+     * 需要鉴权的公共url配置，不需要单独给所有的角色增加的url
+     */
     private List<String> authUrls;
 
 }

5be40e75eb9bf3d31feaf720891ca20cecdcaafe
Melon <wmz1930@users.noreply.github.com>
2022-04-20 18:33:00 +0800

    修复代码生成时数字格式问题

--- gitegg-platform-code-generator/src/main/java/com/gitegg/platform/code/generator/engine/GitEggFreemarkerTemplateEngine.java
+++ gitegg-platform-code-generator/src/main/java/com/gitegg/platform/code/generator/engine/GitEggFreemarkerTemplateEngine.java
@@ -1,18 +1,47 @@
 package com.gitegg.platform.code.generator.engine;
 
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.config.ConstVal;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Map;
 
 /**
  * Freemarker 自定义输出自定义模板文件
+ * 重写父类方法是为了解决数字加逗号的问题，是配置文件可配置
  *
  * @author GitEgg
  * @since 2021-10-12
  */
 public class GitEggFreemarkerTemplateEngine extends FreemarkerTemplateEngine {
+    
+    private Configuration configuration;
+    
+    @Override
+    public @NotNull GitEggFreemarkerTemplateEngine init(@NotNull ConfigBuilder configBuilder) {
+        configuration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+        configuration.setDefaultEncoding(ConstVal.UTF8);
+        configuration.setNumberFormat(StringPool.ZERO);
+        configuration.setClassForTemplateLoading(FreemarkerTemplateEngine.class, StringPool.SLASH);
+        return this;
+    }
+    
+    
+    @Override
+    public void writer(@NotNull Map<String, Object> objectMap, @NotNull String templatePath, @NotNull File outputFile) throws Exception {
+        Template template = configuration.getTemplate(templatePath);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+            template.process(objectMap, new OutputStreamWriter(fileOutputStream, ConstVal.UTF8));
+        }
+    }
 
     /**
      * 自定义输出自定义模板文件

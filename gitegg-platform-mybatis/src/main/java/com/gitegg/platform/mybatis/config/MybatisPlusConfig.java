87f86fc24411c3c0d6321242260eaa499607f54f
Melon <wmz1930@users.noreply.github.com>
2022-06-10 10:34:13 +0800

    新增xxl-job定时任务基础配置

--- gitegg-platform-mybatis/src/main/java/com/gitegg/platform/mybatis/config/MybatisPlusConfig.java
+++ gitegg-platform-mybatis/src/main/java/com/gitegg/platform/mybatis/config/MybatisPlusConfig.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@MapperScan("com.gitegg.**.mapper.**")
+@MapperScan(basePackages  = {"com.**.**.mapper.**"})
 public class MybatisPlusConfig {
 
     private final TenantLineInnerInterceptor tenantLineInnerInterceptor;

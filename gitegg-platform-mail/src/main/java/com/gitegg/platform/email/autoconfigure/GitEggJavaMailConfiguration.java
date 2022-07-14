0f40085b913a5431c0df0e3a4942874aa66919fc
jeebase <jeebase@jeebase.com>
2022-07-14 15:49:02 +0800

    增加邮件配置

--- gitegg-platform-mail/src/main/java/com/gitegg/platform/email/autoconfigure/GitEggJavaMailConfiguration.java
+++ gitegg-platform-mail/src/main/java/com/gitegg/platform/email/autoconfigure/GitEggJavaMailConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,6 +14,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 /**
  * <p>
  * GitEggJavaMail 自动装配类，扩展默认的MailProperties配置类，
+ * 只有开启了多租户，并且邮件也开启了多租户时才启用
  * </p>
  *
  * @author GitEgg
@@ -20,6 +22,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@ConditionalOnProperty( value = {"spring.mail.tenant", "tenant.enable"}, havingValue = "true")
 public class GitEggJavaMailConfiguration {
 
     private final JavaMailSenderImpl javaMailSenderImpl;
